6cbf331ca28912678ea71d0ec340274ed75c40b7
Jonathan Jara <jonathan.jara.2000@gmail.com>
2024-12-05 22:50:08 -0800

    all of interp... i think

--- AAQZ8.groovy
+++ AAQZ8.groovy
@@ -67,6 +67,9 @@ class boolV extends Value {
     boolV(boolean bool) {
         this.bool = bool;
     }
+    String toString() {
+        return bool.toString();
+    }
 }
 
 class strV extends Value {
@@ -111,12 +114,20 @@ class envC {
     }
 }
 
-bindC[] bindArray = new bindC[4]
-
+bindC[] bindArray = new bindC[13]
 bindArray[0] = new bindC("+", new primV("+"))
 bindArray[1] = new bindC("-", new primV("-"))
 bindArray[2] = new bindC("*", new primV("*"))
 bindArray[3] = new bindC("/", new primV("/"))
+bindArray[4] = new bindC("<=", new primV("<="))
+bindArray[5] = new bindC("equal?", new primV("equal?"))
+bindArray[6] = new bindC("true", new boolV(true))
+bindArray[7] = new bindC("false", new boolV(false))
+bindArray[8] = new bindC("println", new primV("println"))
+bindArray[9] = new bindC("read-num", new primV("read-num"))
+bindArray[10] = new bindC("read-str", new primV("read-str"))
+bindArray[11] = new bindC("seq", new primV("seq"))
+bindArray[12] = new bindC("++", new primV("++"))
 
 def topEnv = new envC(bindArray)
 
@@ -209,9 +220,70 @@ def interp (ast, env) {
                         if (right.num == 0) {
                             throw new Exception("Division by zero");
                         }
-                        return new numV(left.num / right.num);
+                        int result = left.num / right.num;
+                        return new numV(result);
                     } else {
                         throw new Exception("Non-numeric argument to /: " + left + " or " + right);
+                    }
+                } else {
+                    throw new Exception("Wrong number of arguments: " + ast.args.length + " instead of 2");
+                }
+            } else if (fun.name == "<=") {
+                if (ast.args.length == 2) {
+                    def left = interp(ast.args[0], env);
+                    def right = interp(ast.args[1], env);
+                    if (left instanceof numV && right instanceof numV) {
+                        return new boolV(left.num <= right.num);
+                    } else {
+                        throw new Exception("Non-numeric argument to <=: " + left + " or " + right);
+                    }
+                } else {
+                    throw new Exception("Wrong number of arguments: " + ast.args.length + " instead of 2");
+                }
+            } else if (fun.name == "equal?") {
+                if (ast.args.length == 2) {
+                    def left = interp(ast.args[0], env);
+                    def right = interp(ast.args[1], env);
+                    return new boolV(left == right);
+                } else {
+                    throw new Exception("Wrong number of arguments: " + ast.args.length + " instead of 2");
+                }
+            } else if (fun.name == "println") {
+                if (ast.args.length == 1) {
+                    def arg = interp(ast.args[0], env);
+                    println(arg);
+                    return new boolV(true);
+                } else {
+                    throw new Exception("Wrong number of arguments: " + ast.args.length + " instead of 1");
+                }
+            } else if (fun.name == "read-num") {
+                if (ast.args.length == 0) {
+                    def input = System.console().readLine();
+                    return new numV(Integer.parseInt(input));
+                } else {
+                    throw new Exception("Wrong number of arguments: " + ast.args.length + " instead of 0");
+                }
+            } else if (fun.name == "read-str") {
+                if (ast.args.length == 0) {
+                    def input = System.console().readLine();
+                    return new strV(input);
+                } else {
+                    throw new Exception("Wrong number of arguments: " + ast.args.length + " instead of 0");
+                }
+            } else if (fun.name == "seq") {
+                def result = new boolV(true);
+                for (int i = 0; i < ast.args.length; i++) {
+                    result = interp(ast.args[i], env);
+                }
+                return result;
+            } else if (fun.name == "++") {
+                if (ast.args.length == 2) {
+                    def left = interp(ast.args[0], env);
+                    def right = interp(ast.args[1], env);
+                    if (left instanceof strV && right instanceof strV) {
+                        return new strV(left.str + right.str);
+                    } else {
+                        throw new Exception("Non-string argument to ++: " + left + " or " + right);
                     }
                 } else {
                     throw new Exception("Wrong number of arguments: " + ast.args.length + " instead of 2");
@@ -274,3 +346,67 @@ if (interp(ast6, topEnv).toString() == "0") {
 } else {
     println("Test 6 Failed")
 }
+
+def ast7 = new appC(new idC("<="), list1)
+if (interp(ast7, topEnv).toString() == "true") {
+    println("Test 7 Passed")
+} else {
+    println("Test 7 Failed")
+}
+
+def ast8 = new appC(new idC("equal?"), list1)
+if (interp(ast8, topEnv).toString() == "false") {
+    println("Test 8 Passed")
+} else {
+    println("Test 8 Failed")
+}
+
+def list2 = new ExprC[1]
+list2[0] = new strC("hi")
+def ast9 = new appC(new idC("println"), list2)
+if (interp(ast9, topEnv).toString() == "true") {
+    println("Test 9 Passed")
+} else {
+    println("Test 9 Failed")
+}
+
+def ast10 = new appC(new idC("read-num"), new ExprC[0])
+println("Enter a number:")
+if (interp(ast10, topEnv) instanceof numV) {
+    println("Test 10 Passed")
+} else {
+    println("Test 10 Failed")
+}
+
+def ast11 = new appC(new idC("read-str"), new ExprC[0])
+println("Enter a string:")
+if (interp(ast11, topEnv) instanceof strV) {
+    println("Test 11 Passed")
+} else {
+    println("Test 11 Failed")
+}
+
+def list3 = new ExprC[2]
+def list4 = new ExprC[1]
+list4[0] = new strC("hello ")
+def list5 = new ExprC[1]
+list5[0] = new strC("world")
+list3[0] = (new appC(new idC("println"), list4))
+list3[1] = (new appC(new idC("println"), list5))
+def ast12 = new appC(new idC("seq"), list3)
+if (interp(ast12, topEnv).toString() == "true") {
+    println("Test 12 Passed")
+} else {
+    println("Test 12 Failed")
+}
+
+def list6 = new ExprC[2]
+list6[0] = new strC("hello ")
+list6[1] = new strC("world")
+def ast13 = new appC(new idC("++"), list6)
+if (interp(ast13, topEnv).toString() == "hello world") {
+    println("Test 13 Passed")
+} else {
+    println("Test 13 Failed")
+}
+
